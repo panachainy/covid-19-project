d476669c433d07b8e42d70d58ca5f96dd9332eb1
Panachai <panachai.ny@gmail.com>
2022-04-01 23:39:01 +0700

    add test client

--- internal/covid/covidclient/dto.go
+++ internal/covid/covidclient/dto.go
@@ -3,18 +3,20 @@ package covidclient
 import "gopkg.in/guregu/null.v4"
 
 type Covid19 struct {
-	Data []struct {
-		ConfirmDate    string      `json:"ConfirmDate"`
-		No             interface{} `json:"No"`
-		Age            null.Int    `json:"Age"`
-		Gender         string      `json:"Gender"`
-		GenderEn       string      `json:"GenderEn"`
-		Nation         interface{} `json:"Nation"`
-		NationEn       string      `json:"NationEn"`
-		Province       null.String `json:"Province"`
-		ProvinceID     int         `json:"ProvinceId"`
-		District       interface{} `json:"District"`
-		ProvinceEn     string      `json:"ProvinceEn"`
-		StatQuarantine int         `json:"StatQuarantine"`
-	} `json:"Data"`
+	Data []Covid19Data `json:"Data"`
+}
+
+type Covid19Data struct {
+	ConfirmDate    string      `json:"ConfirmDate"`
+	No             interface{} `json:"No"`
+	Age            null.Int    `json:"Age"`
+	Gender         string      `json:"Gender"`
+	GenderEn       string      `json:"GenderEn"`
+	Nation         interface{} `json:"Nation"`
+	NationEn       string      `json:"NationEn"`
+	Province       null.String `json:"Province"`
+	ProvinceID     int         `json:"ProvinceId"`
+	District       interface{} `json:"District"`
+	ProvinceEn     string      `json:"ProvinceEn"`
+	StatQuarantine int         `json:"StatQuarantine"`
 }
